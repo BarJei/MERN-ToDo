ffffae631daf1b214788c517a185318851408ff8
Jeirene Richmond Loba Barbo <j.barbo@samsung.com>
2020-11-10 18:34:51 +0800

    updated react codes to use react hooks

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -2,32 +2,32 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Input = ({ getToDos }) => {
-    const [state, setState] = useState({ action: '' });
+    const [action, setAction] = useState('');
 
     const handleChange = e => {
-        setState({ ...state, action: e.target.value });
+        setAction(e.target.value);
     };
 
     const addToDo = () => {
-        const task = { action: state.action };
+        const task = { action };
         if (task.action && task.action.length > 0) {
             axios.post('/api/todos', task)
                 .then(res => {
                     if (res.data) {
                         getToDos();
-                        setState({ ...state, action: '' });
+                        setAction('');
                     }
                 })
-                .catch(err => console.log(err))
+                .catch(err => console.log(err));
         } else {
-            console.log('input field required')
+            console.log('input field required');
         }
     };
 
     return (
         <div>
-            <input type="text" onChange={handleChange} value={state.action} />
-            <button onClick={addToDo}>add todo</button>
+            <input type="text" onChange={handleChange} value={action} />
+            <button onClick={addToDo}>Add</button>
         </div >
     );
 };
--- client/src/components/ToDo.js
+++ client/src/components/ToDo.js
@@ -1,53 +1,43 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Input from './Input';
 import ListToDo from './ListToDo';
 
-class ToDo extends Component {
+const ToDo = () => {
+    const [toDos, setToDos] = useState([]);
 
-  state = {
-    todos: []
-  }
+    useEffect(() => {
+        getToDos();
+    }, []);
 
-  componentDidMount(){
-    this.getTodos();
-  }
+    const getToDos = () => {
+        axios.get('/api/todos')
+            .then(res => {
+                if (res.data) {
+                    setToDos(res.data);
+                }
+            })
+            .catch(err => console.log(err));
+    };
 
-  getTodos = () => {
-    axios.get('/api/todos')
-      .then(res => {
-        if(res.data){
-          this.setState({
-            todos: res.data
-          })
-        }
-      })
-      .catch(err => console.log(err))
-  }
+    const deleteToDo = id => {
+        axios.delete(`/api/todos/${id}`)
+            .then(res => {
+                if (res.data) {
+                    getToDos();
+                }
+            })
+            .catch(err => console.log(err))
+    };
 
-  deleteTodo = (id) => {
-
-    axios.delete(`/api/todos/${id}`)
-      .then(res => {
-        if(res.data){
-          this.getTodos()
-        }
-      })
-      .catch(err => console.log(err))
-  }
-
-  render() {
-    let { todos } = this.state;
-
-    return(
-      <div>
-        <h1>My Todo(s)</h1>
-        <Input getToDos={this.getTodos}/>
-        <ListToDo todos={todos} deleteTodo={this.deleteTodo}/>
-      </div>
+    return (
+        <div>
+            <h1>My To Do(s)</h1>
+            <Input getToDos={getToDos} />
+            <ListToDo toDos={toDos} deleteToDo={deleteToDo} />
+        </div>
     )
-  }
-}
+};
 
 export default ToDo;
